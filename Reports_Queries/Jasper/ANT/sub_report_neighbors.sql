3d93c75e8e970726db57786a2647bbb9381f02ae
lacardonap <leo.cardona.p@gmail.com>
2020-08-06 10:33:32 -0500

    Update report and queries for ANT

--- Reports_Queries/Jasper/ANT/sub_report_neighbors.sql
+++ Reports_Queries/Jasper/ANT/sub_report_neighbors.sql
@@ -1,29 +1,43 @@
-WITH 
+WITH
 parametros AS (
   SELECT
-     1522 	AS poligono_t_id, --$P{id} 
-       	4	AS criterio_punto_inicial, --tipo de criterio para seleccionar el punto inicial del terreno, valores posibles: 1,2 $P{criterio_punto_inicial}
-    4		AS criterio_observador, --1: Centroide, 2: Centro del extent, 3: punto en la superficie, 4: Punto mas cercano al centroide dentro del poligono
-    true	AS incluir_tipo_derecho --Mostrar el tipo de derecho de cada interesado (booleano)
+    1432 	AS terreno_t_id, --$P{id}
+     2 		AS criterio_punto_inicial, --tipo de criterio para seleccionar el punto inicial del terreno, valores posibles: 1,2 parametrizar $P{criterio_punto_inicial}
+     4		AS criterio_observador, --1: Centroide, 2: Centro del extent, 3: punto en la superficie, 4: Punto mas cercano al centroide dentro del poligono
+	true	AS incluir_tipo_derecho --Mostrar el tipo de derecho de cada interesado (booleano)
 ),
-t AS ( --Orienta los vertices del terreno en sentido horario
-	SELECT t_id, ST_ForceRHR(geometria) as geometria FROM ladm_lev_cat_v1.lc_terreno AS t, parametros WHERE t.t_id = poligono_t_id --$P!{datasetName}
+t AS (
+	SELECT t_id, ST_ForceRHR(geometria) AS geometria FROM ladm_lev_cat_v1.lc_terreno WHERE t_id = (SELECT terreno_t_id FROM parametros)
+),
+linderos AS (
+	SELECT lc_lindero.t_id, lc_lindero.geometria AS geom  FROM ladm_lev_cat_v1.lc_lindero JOIN ladm_lev_cat_v1.col_masccl ON col_masccl.ue_mas_lc_terreno = (SELECT terreno_t_id FROM parametros) AND lc_lindero.t_id = col_masccl.ccl_mas
+),
+terrenos_asociados_linderos AS (
+	SELECT DISTINCT col_masccl.ue_mas_lc_terreno AS t_id_terreno, col_masccl.ccl_mas AS t_id_lindero FROM ladm_lev_cat_v1.col_masccl WHERE col_masccl.ccl_mas IN (SELECT t_id FROM linderos) AND col_masccl.ue_mas_lc_terreno != (SELECT terreno_t_id FROM parametros)
+),
+puntos_lindero AS (
+	SELECT lc_puntolindero.t_id, lc_puntolindero.geometria AS geom FROM ladm_lev_cat_v1.lc_puntolindero JOIN ladm_lev_cat_v1.col_puntoccl ON col_puntoccl.ccl IN (SELECT t_id FROM linderos) AND lc_puntolindero.t_id = col_puntoccl.punto_lc_puntolindero
+),
+puntos_terreno AS (
+	SELECT (ST_DumpPoints(geometria)).* AS dp,
+			ST_NPoints(geometria) total
+	FROM t
 ),
 --bordes de la extension del poligono
-a AS (
+punto_nw AS (
 	SELECT ST_SetSRID(ST_MakePoint(st_xmin(t.geometria), st_ymax(t.geometria)), ST_SRID(t.geometria)) AS p FROM t
 ),
-b AS (
+punto_ne AS (
 	SELECT ST_SetSRID(ST_MakePoint(st_xmax(t.geometria), st_ymax(t.geometria)), ST_SRID(t.geometria)) AS p FROM t
 ),
-c AS (
+punto_se AS (
 	SELECT ST_SetSRID(ST_MakePoint(st_xmax(t.geometria), st_ymin(t.geometria)), ST_SRID(t.geometria)) AS p FROM t
 ),
-d AS (
+punto_sw AS (
 	SELECT ST_SetSRID(ST_MakePoint(st_xmin(t.geometria), st_ymin(t.geometria)), ST_SRID(t.geometria)) AS p FROM t
 ),
 --Punto medio (ubicación del observador para la definicion de las cardinalidades)
-m AS (
+punto_medio AS (
   SELECT
     CASE WHEN criterio_observador = 1 THEN --centroide del poligono
       ( SELECT ST_SetSRID(ST_MakePoint(st_x(ST_centroid(t.geometria)), st_y(ST_centroid(t.geometria))), ST_SRID(t.geometria)) AS p FROM t )
@@ -35,112 +49,56 @@ m AS (
       ( SELECT ST_SetSRID(ST_MakePoint(st_x( ST_ClosestPoint( geometria, ST_centroid(t.geometria))), st_y( ST_ClosestPoint( geometria,ST_centroid(t.geometria)))), ST_SRID(t.geometria)) AS p FROM t )
     ELSE --defecto: Centro del extent
       ( SELECT ST_SetSRID(ST_MakePoint(st_x(ST_centroid(st_envelope(t.geometria))), st_y(ST_centroid(st_envelope(t.geometria)))), ST_SRID(t.geometria)) AS p FROM t )
-    END as p
+    END AS p
     FROM parametros
 ),
---Cuadrantes del polígono desde el observador a cada una de las esquinas de la extensión del polígono
-norte AS (
-	SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(ARRAY [a.p, b.p, m.p, a.p])), ST_SRID(t.geometria)) geom FROM t,a,b,m
+cuadrante_norte AS (
+	SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(ARRAY [punto_nw.p, punto_ne.p, punto_medio.p, punto_nw.p])), ST_SRID(t.geometria)) geom FROM t, punto_nw, punto_ne, punto_medio
 ),
-este AS (
-	SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(ARRAY [m.p, b.p, c.p, m.p])), ST_SRID(t.geometria)) geom FROM t,b,c,m
+cuadrante_este AS (
+	SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(ARRAY [punto_medio.p, punto_ne.p, punto_se.p, punto_medio.p])), ST_SRID(t.geometria)) geom FROM t,punto_ne,punto_se,punto_medio
 ),
-sur AS (
-	SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(ARRAY [m.p, c.p, d.p, m.p])), ST_SRID(t.geometria)) geom FROM t,m,c,d
+cuadrante_sur AS (
+	SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(ARRAY [punto_medio.p, punto_se.p, punto_sw.p, punto_medio.p])), ST_SRID(t.geometria)) geom FROM t,punto_medio,punto_se,punto_sw
 ),
-oeste AS (
-	SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(ARRAY [a.p, m.p, d.p, a.p])), ST_SRID(t.geometria)) geom FROM t,a,m,d
-)
-,limite_poligono as(
-	SELECT t_id, ST_Boundary(geometria) geom FROM t
-)
-,limite_vecinos as (  --obtiene el limite de los terrenos colindantes, filtrados por bounding box
-	select o.t_id, ST_Boundary(o.geometria) geom from t, ladm_lev_cat_v1.lc_terreno o where o.geometria && st_envelope(t.geometria) and t.t_id <> o.t_id --$P!{datasetName}
-)
-,pre_colindancias as ( --inteseccion entre el limite del poligono y los terrenos cercanos, añade la geometria de los limites sin adjacencia
-	SELECT limite_vecinos.t_id, st_intersection(limite_poligono.geom,limite_vecinos.geom) geom  FROM limite_poligono,limite_vecinos where st_intersects(limite_poligono.geom,limite_vecinos.geom) and limite_poligono.t_id <> limite_vecinos.t_id
-	union 
-	SELECT null as t_id, ST_Difference(limite_poligono.geom, a.geom) geom
-	FROM limite_poligono,
-	(
-		select ST_LineMerge(ST_Union(geom)) geom from limite_vecinos
-	) a 
-)
-, tmp_colindantes as (
-	select  t_id,ST_LineMerge(ST_Union(geom)) geom from 
-	(
-		SELECT
-		  simple.t_id,
-		  simple.simple_geom as geom,
-		  ST_GeometryType(simple.simple_geom) as geom_type,
-		  ST_AsEWKT(simple.simple_geom) as geom_wkt
-		FROM (
-		  SELECT
-		    dumped.*,
-		    (dumped.geom_dump).geom as simple_geom,
-		    (dumped.geom_dump).path as path
-		  FROM (
-		    SELECT *, ST_Dump(geom) AS geom_dump FROM pre_colindancias
-		  ) as dumped
-		) AS simple
-	) a
-	group by t_id
-)
-, lineas_colindancia as ( --contiene las lineas de cambio de colindancia todas las lineas son parte simple
-	SELECT * FROM
-	(
-		SELECT
-		  simple.t_id,
-		  simple.simple_geom as geom
-		FROM (
-		  SELECT
-		    dumped.*,
-		    (dumped.geom_dump).geom as simple_geom,
-		    (dumped.geom_dump).path as path
-		  FROM (
-		    SELECT *, ST_Dump(geom) AS geom_dump FROM (select * from tmp_colindantes where ST_GeometryType(geom) = 'ST_MultiLineString') a
-		  ) as dumped
-		) AS simple			
-	) a 
-	UNION 
-	select * from tmp_colindantes where ST_GeometryType(geom) <> 'ST_MultiLineString'
-)
-, puntos_terreno as (
-	SELECT (ST_DumpPoints(geometria)).* AS dp
-	FROM t
-)
---Criterio 1: el punto inicial del terreno es el primer punto del lindero que intersecte con el punto ubicado mas cerca de la esquina nw del polígono
-, punto_nw as (
-	SELECT 	geom
-		,st_distance(geom, nw) AS dist
-	FROM 	puntos_terreno,
-		(SELECT ST_SetSRID(ST_MakePoint(st_xmin(st_envelope(geometria)), st_ymax(st_envelope(geometria))), ST_SRID(geometria)) as nw FROM t ) a
-	ORDER BY dist limit 1
-)
-, punto_inicial_por_lindero_con_punto_nw as (
-	select st_startpoint(lineas_colindancia.geom) geom from lineas_colindancia, punto_nw where st_intersects(lineas_colindancia.geom, punto_nw.geom ) and not st_intersects(st_endpoint(lineas_colindancia.geom), punto_nw.geom )  limit 1
-)
---Criterio 2: el punto inicial del terreno es el primer punto del lindero que tenga mayor porcentaje de su longitud sobre el cuadrante norte del poligono
-, punto_inicial_por_lindero_porcentaje_n as(
-	select 	round((st_length(st_intersection(lineas_colindancia.geom, norte.geom))/st_length(lineas_colindancia.geom))::numeric,2) dist, 
-		st_startpoint(lineas_colindancia.geom) geom 
-		,st_distance(lineas_colindancia.geom,nw) distance_to_nw
-		from lineas_colindancia
-			,norte
-			,(SELECT ST_SetSRID(ST_MakePoint(st_xmin(st_envelope(geometria)), st_ymax(st_envelope(geometria))), ST_SRID(geometria)) as nw FROM t ) a
-		where st_intersects(lineas_colindancia.geom, norte.geom)  order by dist desc, distance_to_nw
-		limit 1
-)
---Criterio para definir el punto inicial del terreno
-,punto_inicial as (
-	SELECT 
-		CASE WHEN criterio_punto_inicial = 1 THEN (select geom from punto_inicial_por_lindero_con_punto_nw)
-		WHEN criterio_punto_inicial = 2 THEN (select geom from punto_inicial_por_lindero_porcentaje_n)
-	END as geom
+cuadrante_oeste AS (
+	SELECT ST_SetSRID(ST_MakePolygon(ST_MakeLine(ARRAY [punto_nw.p, punto_medio.p, punto_sw.p, punto_nw.p])), ST_SRID(t.geometria)) geom FROM t,punto_nw,punto_medio,punto_sw
+),
+cuadrantes AS (
+	SELECT 'Norte' ubicacion, geom AS cuadrante FROM cuadrante_norte
+	UNION
+	SELECT 'Este' ubicacion, geom AS cuadrante FROM cuadrante_este
+	UNION
+	SELECT 'Sur' ubicacion, geom AS cuadrante FROM cuadrante_sur
+	UNION
+	SELECT 'Oeste' ubicacion, geom AS cuadrante FROM cuadrante_oeste
+),
+punto_inicial_por_lindero_porcentaje_n AS (
+	SELECT 	round((st_length(st_intersection(linderos.geom, cuadrante_norte.geom))/st_length(linderos.geom))::numeric,2) dist,
+		st_startpoint(linderos.geom) geom
+		,st_distance(linderos.geom, punto_nw.p) distance_to_nw
+		FROM linderos
+			,cuadrante_norte
+			, punto_nw
+		WHERE st_intersects(linderos.geom, cuadrante_norte.geom)  ORDER BY dist DESC, distance_to_nw
+		LIMIT 1
+),
+punto_inicial_por_lindero_con_punto_nw AS (
+	SELECT 	geom,
+			st_distance(geom, nw) AS dist
+	FROM puntos_terreno,
+		(SELECT ST_SetSRID(ST_MakePoint(st_xmin(st_envelope(geometria)), st_ymax(st_envelope(geometria))), ST_SRID(geometria)) AS nw FROM t) a
+	ORDER BY dist LIMIT 1
+),
+punto_inicial AS (
+	SELECT
+		CASE WHEN criterio_punto_inicial = 1 THEN (SELECT geom FROM punto_inicial_por_lindero_con_punto_nw)
+		WHEN criterio_punto_inicial = 2 THEN (SELECT geom FROM punto_inicial_por_lindero_porcentaje_n)
+	END AS geom
 	FROM parametros
-)
-, puntos_ordenados as (
-	SELECT case when id-m+1 <= 0 then total + id-m else id-m+1 end as id, geom , st_x(geom) x, st_y(geom) y FROM
+),
+puntos_terreno_ordenados AS (
+	SELECT CASE WHEN id-m+1 <= 0 THEN total + id-m ELSE id-m+1 END AS id, geom  FROM
 		(
 		SELECT row_number() OVER (ORDER BY path) AS id
 			,m
@@ -148,7 +106,7 @@ oeste AS (
 			,geom
 			,total
 		FROM (
-			SELECT (ST_DumpPoints(ST_ForceRHR(geometria))).* AS dp
+			SELECT (ST_DumpPoints(geometria)).* AS dp
 				,ST_NPoints(geometria) total
 				,geometria
 			FROM t
@@ -157,75 +115,80 @@ oeste AS (
 				SELECT row_number() OVER (ORDER BY path) AS m
 					,st_distance(puntos_terreno.geom, punto_inicial.geom) AS dist
 				FROM puntos_terreno,punto_inicial
-				ORDER BY dist limit 1
+				ORDER BY dist LIMIT 1
 			) b
 		) t
-		where id <> total
-	order by id
-)
-, cuadrantes as (
-	SELECT 'Norte' ubicacion,norte.geom as cuadrante FROM norte 
-	UNION
-	SELECT 'Este' ubicacion,este.geom as cuadrante FROM este 
-	UNION
-	SELECT 'Sur' ubicacion,sur.geom as cuadrante FROM sur 
-	UNION
-	SELECT 'Oeste' ubicacion,oeste.geom as cuadrante FROM oeste
-)
-, lineas_colindancia_desde_hasta as (
-	select *
-		,(SELECT id from puntos_ordenados WHERE st_intersects(puntos_ordenados.geom, st_startpoint(lineas_colindancia.geom))) desde
-		,(SELECT id from puntos_ordenados WHERE st_intersects(puntos_ordenados.geom, st_endpoint(lineas_colindancia.geom))) hasta
-	from lineas_colindancia
-	order by desde
-)
-, colindantes as (
-	SELECT row_number() OVER (ORDER BY desde) AS id, t_id,desde,hasta,ubicacion,geom FROM
+		WHERE id <> total
+	ORDER BY id
+),
+puntos_lindero_ordenados AS (
+    SELECT * FROM (
+        SELECT DISTINCT ON (t_id) t_id, id, st_distance(puntos_lindero.geom, puntos_terreno_ordenados.geom) AS distance, puntos_lindero.geom, st_x(puntos_lindero.geom) x, st_y(puntos_lindero.geom) y
+        FROM puntos_lindero, puntos_terreno_ordenados ORDER BY t_id, distance
+        LIMIT (SELECT count(DISTINCT t_id) FROM puntos_lindero)
+    ) tmp_puntos_lindero_ordenados ORDER BY id
+),
+linderos_desde_hasta AS (
+	SELECT *
+		, (SELECT id FROM (
+			SELECT id, st_distance(puntos_lindero_ordenados.geom, st_startpoint(linderos.geom)) AS distance FROM puntos_lindero_ordenados ORDER BY distance ASC LIMIT 1
+		) AS tmp_desde) desde
+		, (SELECT id FROM (
+			SELECT id, st_distance(puntos_lindero_ordenados.geom, st_endpoint(linderos.geom)) AS distance FROM puntos_lindero_ordenados ORDER BY distance ASC LIMIT 1
+		) AS tmp_hasta) hasta
+	FROM linderos
+	ORDER BY desde
+),
+linderos_colindantes AS (
+	SELECT row_number() OVER (ORDER BY desde) AS id, t_id AS t_id_linderos, desde, hasta, ubicacion, geom FROM
 	(
-		select * 
-			,st_length(st_intersection(geom,cuadrante))/st_length(geom) as porcentaje 
-			,max(st_length(st_intersection(geom,cuadrante))/st_length(geom)) over (partition by geom) as max_porce
-		from lineas_colindancia_desde_hasta, cuadrantes where st_intersects(geom,cuadrante)
+		SELECT *
+			,st_length(st_intersection(geom,cuadrante))/st_length(geom) AS porcentaje
+			,max(st_length(st_intersection(geom,cuadrante))/st_length(geom)) OVER (partition BY geom) AS max_porce
+		FROM linderos_desde_hasta, cuadrantes WHERE st_intersects(geom,  cuadrante)
 	) a
-	where porcentaje = max_porce
-) 
-SELECT 
+	WHERE porcentaje = max_porce
+),
+colindantes AS (
+	SELECT linderos_colindantes.*, terrenos_asociados_linderos.t_id_terreno  FROM linderos_colindantes JOIN terrenos_asociados_linderos ON linderos_colindantes.t_id_linderos = terrenos_asociados_linderos.t_id_lindero
+)
+SELECT
   id
   ,desde
   ,hasta
   ,ubicacion
   ,nupre
-  ,CASE WHEN numero_predial is null and matricula_inmobiliaria IS NULL and nombre IS NULL THEN 'ÁREA INDETERMINADA'
-    ELSE COALESCE(numero_predial || ';','') || COALESCE('FMI: ' || matricula_inmobiliaria || ';','') || COALESCE('Nombre: ' || nombre ,'') 
-   END as predio
+  ,CASE WHEN numero_predial IS null AND matricula_inmobiliaria IS NULL AND nombre IS NULL THEN 'ÁREA INDETERMINADA'
+    ELSE COALESCE(numero_predial || ';','') || COALESCE('FMI: ' || matricula_inmobiliaria || ';','') || COALESCE('Nombre: ' || nombre ,'')
+   END AS predio
   ,lc_predio.t_id
   ,COALESCE(interesado, 'INDETERMINADO') AS interesado
   ,round(st_length(colindantes.geom)::numeric,2) distancia
 FROM
 colindantes
-LEFT JOIN ladm_lev_cat_v1.lc_terreno ON lc_terreno.t_id = colindantes.t_id --$P!{datasetName}
-LEFT JOIN ladm_lev_cat_v1.col_uebaunit ON colindantes.t_id = ue_lc_terreno --$P!{datasetName}
+LEFT JOIN ladm_lev_cat_v1.lc_terreno ON lc_terreno.t_id = colindantes.t_id_terreno --$P!{datasetName}
+LEFT JOIN ladm_lev_cat_v1.col_uebaunit ON colindantes.t_id_terreno = ue_lc_terreno --$P!{datasetName}
 LEFT JOIN ladm_lev_cat_v1.lc_predio ON lc_predio.t_id = baunit --$P!{datasetName}
 LEFT JOIN
 (
   SELECT t_id,
-	array_to_string(array_agg(( coalesce(primer_nombre,'') || coalesce(' ' || segundo_nombre, '') || coalesce(' ' || primer_apellido, '') || coalesce(' ' || segundo_apellido, '') ) 
-				|| ( coalesce(razon_social, '') ) 
-				|| ', ' || (select dispname from ladm_lev_cat_v1.lc_interesadodocumentotipo where t_id = tipo_documento) || ': ' --$P!{datasetName}
-				|| documento_identidad 
-				|| CASE WHEN (SELECT incluir_tipo_derecho FROM parametros) THEN 
-					' (' || (select dispname from ladm_lev_cat_v1.lc_derechotipo where t_id = tipo_derecho) || ')' --opcional: ver tipo de derecho de cada interesado $P!{datasetName}
+	array_to_string(array_agg(( coalesce(primer_nombre,'') || coalesce(' ' || segundo_nombre, '') || coalesce(' ' || primer_apellido, '') || coalesce(' ' || segundo_apellido, '') )
+				|| ( coalesce(razon_social, '') )
+				|| ', ' || (SELECT dispname FROM ladm_lev_cat_v1.lc_interesadodocumentotipo WHERE t_id = tipo_documento) || ': ' --$P!{datasetName}
+				|| documento_identidad
+				|| CASE WHEN (SELECT incluir_tipo_derecho FROM parametros) THEN
+					' (' || (SELECT dispname FROM ladm_lev_cat_v1.lc_derechotipo WHERE t_id = tipo_derecho) || ')' --opcional: ver tipo de derecho de cada interesado $P!{datasetName}
 				  ELSE '' END
 				) , '; ')
-			  as interesado
-  FROM 
-  (	
+			  AS interesado
+  FROM
+  (
 	--navegar agrupación de interesados
 	SELECT * FROM
 		ladm_lev_cat_v1.lc_predio -- $P!{datasetName}
 		LEFT JOIN
 		(
-			select 
+			SELECT
 			  primer_nombre
 			  ,segundo_nombre
 			  ,primer_apellido
@@ -234,12 +197,12 @@ LEFT JOIN
 			  ,tipo_documento
 			  ,documento_identidad
 			  ,unidad
-			  ,lc_derecho.tipo as tipo_derecho
-			from 
+			  ,lc_derecho.tipo AS tipo_derecho
+			FROM
 			  ladm_lev_cat_v1.lc_derecho --$P!{datasetName}
-			  JOIN ladm_lev_cat_v1.lc_agrupacioninteresados on lc_agrupacioninteresados.t_id = interesado_lc_agrupacioninteresados --$P!{datasetName}
-			  JOIN ladm_lev_cat_v1.col_miembros on agrupacion = lc_agrupacioninteresados.t_id --$P!{datasetName}
-			  JOIN ladm_lev_cat_v1.lc_interesado on lc_interesado.t_id = col_miembros.interesado_lc_interesado --$P!{datasetName}
+			  JOIN ladm_lev_cat_v1.lc_agrupacioninteresados ON lc_agrupacioninteresados.t_id = interesado_lc_agrupacioninteresados --$P!{datasetName}
+			  JOIN ladm_lev_cat_v1.col_miembros ON agrupacion = lc_agrupacioninteresados.t_id --$P!{datasetName}
+			  JOIN ladm_lev_cat_v1.lc_interesado ON lc_interesado.t_id = col_miembros.interesado_lc_interesado --$P!{datasetName}
 		 ) agrupacion  ON lc_predio.t_id = agrupacion.unidad
 	UNION
 	--navegar agrupación de interesados
@@ -247,7 +210,7 @@ LEFT JOIN
 		ladm_lev_cat_v1.lc_predio --$P!{datasetName}
 		LEFT JOIN
 		(
-			select 			
+			SELECT
 			  primer_nombre
 			  ,segundo_nombre
 			  ,primer_apellido
@@ -256,12 +219,12 @@ LEFT JOIN
 			  ,tipo_documento
 			  ,documento_identidad
 			  ,unidad
-			  ,lc_derecho.tipo as tipo_derecho
-			from 
+			  ,lc_derecho.tipo AS tipo_derecho
+			FROM
 			  ladm_lev_cat_v1.lc_derecho --$P!{datasetName}
-			  JOIN ladm_lev_cat_v1.lc_interesado on lc_interesado.t_id =interesado_lc_interesado --$P!{datasetName}
+			  JOIN ladm_lev_cat_v1.lc_interesado ON lc_interesado.t_id =interesado_lc_interesado --$P!{datasetName}
 		) interesado ON lc_predio.t_id = interesado.unidad
   ) interesados
-  group by t_id
-) interesados on interesados.t_id = lc_predio.t_id
+  GROUP BY t_id
+) interesados ON interesados.t_id = lc_predio.t_id
 ORDER BY id
